36212144fa9a500060c94183b8c2e8ad568643a0
Allure1214 <cyx.yongxian01@gmail.com>
2023-07-28 16:35:36 +0800

    Completed

--- src/DiplomaData.java
+++ src/DiplomaData.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class DiplomaData {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class DiplomaData {
+    public String bil;
+    public String category;
+    public String name;
+    public String year2014;
+    public String year2015;
+    public String year2016;
+    public String year2017;
+    public String year2018;
+    public String year2019;
+
+    public DiplomaData(String bil, String category, String name, String year2014, String year2015, String year2016,
+                       String year2017, String year2018, String year2019) {
+
+        this.bil = bil;
+        this.category = category;
+        this.name = name;
+        this.year2014 = year2014;
+        this.year2015 = year2015;
+        this.year2016 = year2016;
+        this.year2017 = year2017;
+        this.year2018 = year2018;
+        this.year2019 = year2019;
+    }
+            public int getYear2014(){
+                return Integer.parseInt(year2014);
+            }
+            public int getYear2015(){
+                return Integer.parseInt(year2015);
+            }
+
+            public int getYear2016(){
+                return Integer.parseInt(year2016);
+            }
+
+            public int getYear2017(){
+                return Integer.parseInt(year2017);
+            }
+
+            public int getYear2018(){
+                return Integer.parseInt(year2018);
+            }
+
+            public int getYear2019(){
+                return Integer.parseInt(year2019);
+            }
+
+            public int getTotal () {
+                return Integer.parseInt(year2014) + Integer.parseInt(year2015) + Integer.parseInt(year2016) +
+                        Integer.parseInt(year2017) + Integer.parseInt(year2018) + Integer.parseInt(year2019);
+            }
+            @Override
+            public String toString () {
+                return bil + ". " + " Category = " + category + ", Name = " + name + ", Year 2014 = " + year2014 +
+                        ", Year 2015 = " + year2015 + ", Year 2016 = " + year2016 + ", Year 2017 = " + year2017 + ", " +
+                        "Year 2018 = " + year2018 + ", Year 2019 = " + year2019;
+            }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,98 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+
 public class Main {
+    public static int Total = 0;
+    public static int Year2014 = 0;
+    public static int Year2015 = 0;
+    public static int Year2016 = 0;
+    public static int Year2017 = 0;
+    public static int Year2018 = 0;
+    public static int Year2019 = 0;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File diploma = new File("diploma.csv");
+
+        int index = 0;
+
+        List<DiplomaData> diplomaLanjutan = new ArrayList<>();
+        List<DiplomaData> kursusPengkhususan = new ArrayList<>();
+
+        System.out.println("The sum of intakes from the year 2014 to 2019 is ");
+
+
+        if(diploma.exists()) {
+            System.out.println("File exists");
+            System.out.println("The file in "+ diploma.getAbsolutePath());
+        }
+
+        else {
+            System.out.println("File don't exists");
+        }
+
+        if(diploma.canRead()) {
+            System.out.println("File is readable");
+        }
+
+        else {
+            System.out.println("File is unreadable");
+        }
+
+        try(Scanner reader = new Scanner(diploma)) {
+            while (reader.hasNext()) {
+                String line = reader.nextLine();
+                if(index > 0) {
+                    String[] items = line.split(",");
+
+                    DiplomaData data = new DiplomaData(items[0], items[1], items[2], items[3], items[4],
+                                                        items[5], items[6], items[7], items[8]);
+
+                    Total = Total + data.getTotal();
+                    Year2014 = Year2014 + data.getYear2014();
+                    Year2015 = Year2015 + data.getYear2015();
+                    Year2016 = Year2016 + data.getYear2016();
+                    Year2017 = Year2017 + data.getYear2017();
+                    Year2018 = Year2018 + data.getYear2018();
+                    Year2019 = Year2019 + data.getYear2019();
+
+                    if (" Diploma Lanjutan".equals(items[1])) {
+                            diplomaLanjutan.add(data);
+                    }
+                    else if ("Kursus Pengkhususan".equals(items[1])) {
+                            kursusPengkhususan.add(data);
+                    }
+
+                        try(PrintWriter writer = new PrintWriter(new File("Diploma Lanjutan.txt"))){
+                            for(DiplomaData row: diplomaLanjutan) {
+                                    writer.println(row);
+                            }
+                        }
+
+                        catch (FileNotFoundException exception) {
+                            System.out.println(exception.getMessage());
+                        }
+
+                    try(PrintWriter writer = new PrintWriter(new File("Kursus Pengkhususan.txt"))){
+                        for(DiplomaData row: kursusPengkhususan) {
+                            writer.println(row);
+                        }
+                    }
+                    catch (FileNotFoundException exception) {
+                        System.out.println(exception.getMessage());
+                    }
+                    }
+                index++;
+                }
+
+            System.out.println("The sum of intakes from the year 2014 to 2019 is " + Total);
+            System.out.println("The minimum number of intakes between the year 2014 to 2019 is " + Math.min(Math.min(Math.min(Math.min(Math.min(Year2014, Year2015), Year2016), Year2017), Year2018), Year2019));
+            System.out.println("The maximum number of intakes between the year 2014 to 2019 is " + Math.max(Math.max(Math.max(Math.max(Math.max(Year2014, Year2015), Year2016), Year2017), Year2018), Year2019));
+
+            }
+        catch (FileNotFoundException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
